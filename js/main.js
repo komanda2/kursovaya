39e3147bd60f83330cd7e5ef0485664403d6b4ea
vdmlnk <vdmlnk98@gmail.com>
2018-12-04 13:38:53 +0300

    email
    test

--- js/main.js
+++ js/main.js
@@ -894,29 +894,7 @@ License: https://themeforest.net/licenses/standard
 
 
 	// Contact Form
-	$(function($){
-							$("#contact").submit(function(event){
-								event.preventDefault();
 
-								$.ajax({
-						    url: "https://formspree.io/komandapinf@gmail.com",
-						    method: "POST",
-						    data: {
-									name: $("#name").val(),
-									email: $("#email").val(),
-									message: $("#message").val()
-								},
-						    dataType: "json"
-								}).done(function(){
-									$("#name").val("");
-									$("#email").val("");
-									$("#message").val("");
-									alert("Сообщение доставлено!");
-								}).fail(function(){
-									alert("Произошла ошибка!")
-								});
-							});
-						});
 
 
 	// window load function
